ff556f94280a83e7987bed754f99b2e7f82b2858
iamrafehdev <abdulrafeh46@gmail.com>
2022-11-17 23:50:39 +0500

    setup main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {config} from 'dotenv';
+
+const dotenv = config()
+const port = process.env.PORT || 3002
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+    const app = await NestFactory.create(AppModule, {cors: true});
+
+    app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept, Authorization',
+        credentials: true,
+    });
+    await app.listen(port);
 }
+
 bootstrap();
