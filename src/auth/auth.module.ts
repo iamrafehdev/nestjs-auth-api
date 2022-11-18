ff398ef2d9d3be817977df4f0b59f317cd42507a
iamrafehdev <abdulrafeh46@gmail.com>
2022-11-19 00:12:21 +0500

    setup AUTH

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: jwtConstants.expiresIn },
+        }),
+    ],
+    providers: [AuthService, JwtStrategy]
+})
+export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+    secret: 'jwt@auth-api',
+    expiresIn: '365d',
+};
